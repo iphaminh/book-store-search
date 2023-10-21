58f95cd820551dfffa2c4e72cbab6f2350846b21
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-10-21 18:55:27 -0400

    set up login

--- Develop/client/src/components/LoginForm.jsx
+++ Develop/client/src/components/LoginForm.jsx
@@ -1,24 +1,35 @@
-// see SignupForm.js for comments
-import { useState } from 'react';
+// Import necessary libraries and dependencies
+import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+// Import the required Apollo hooks and mutation
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+
+// Import the Auth utility for user authentication
 import Auth from '../utils/auth';
 
+// Define the LoginForm component
 const LoginForm = () => {
+  // Initialize state for user login form data, form validation, and error alert
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
+  // Use the LOGIN_USER mutation with Apollo Client
+  const [loginUser, { error }] = useMutation(LOGIN_USER);
+
+  // Handle input changes in the form fields
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
   };
 
+  // Handle form submission when the user tries to log in
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    // check if form has everything (as per react-bootstrap docs)
+    // Check if the form is valid (as per react-bootstrap docs)
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
@@ -26,33 +37,38 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      // Use the loginUser mutation to log in the user and get a token
+      const { data } = await loginUser({
+        variables: { ...userFormData },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      // Log the data received from the server and store the token in Auth
+      console.log(data);
+      Auth.login(data.login.token);
     } catch (err) {
+      // Handle any errors by displaying an error alert
       console.error(err);
       setShowAlert(true);
     }
 
+    // Reset the form data
     setUserFormData({
-      username: '',
       email: '',
       password: '',
     });
   };
 
+  // Render the login form with email and password fields
   return (
     <>
+      {/* Render the login form */}
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        {/* Display an alert for login errors */}
         <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
           Something went wrong with your login credentials!
         </Alert>
+
+        {/* Email input field */}
         <Form.Group className='mb-3'>
           <Form.Label htmlFor='email'>Email</Form.Label>
           <Form.Control
@@ -66,6 +82,7 @@ const LoginForm = () => {
           <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
         </Form.Group>
 
+        {/* Password input field */}
         <Form.Group className='mb-3'>
           <Form.Label htmlFor='password'>Password</Form.Label>
           <Form.Control
@@ -78,6 +95,8 @@ const LoginForm = () => {
           />
           <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
         </Form.Group>
+
+        {/* Submit button */}
         <Button
           disabled={!(userFormData.email && userFormData.password)}
           type='submit'
@@ -89,4 +108,5 @@ const LoginForm = () => {
   );
 };
 
+// Export the LoginForm component
 export default LoginForm;
