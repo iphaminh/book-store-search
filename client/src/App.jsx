17e9b39b49fe368153c276129a2af5a8f20c985d
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-10-22 20:27:03 -0400

    change back old app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,52 +1,36 @@
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
-import LoginForm from './components/LoginForm';
-import SignupForm from './components/SignupForm';
-import process from "process";
 
-
-const httpLink = createHttpLink({
-  uri: process.env.NODE_ENV === 'production'
-    ? 'https://book-engine-v1-508355d22e8a.herokuapp.com/graphql'
-    : 'http://localhost:3001',
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-
-
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
+// Configure Apollo Client
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  uri: "/graphql", // The URI to the GraphQL server endpoint
   cache: new InMemoryCache(),
+  headers: {
+    authorization: localStorage.getItem("id_token") ? `Bearer ${localStorage.getItem("id_token")}` : "",
+  },
 });
 
 function App() {
   return (
+    // Wrap the entire application with ApolloProvider to enable Apollo Client functionality for components
     <ApolloProvider client={client}>
+      {/* Use Router component for client-side routing */}
       <Router>
-        <>
-          <Navbar />
-          <Switch>
-            <Route exact path='/' component={SearchBooks} />
-            <Route exact path='/saved' component={SavedBooks} />
-            <Route exact path='/login' component={LoginForm} />
-            <Route exact path='/signup' component={SignupForm} />
-            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-          </Switch>
-        </>
+        {/* Display the Navbar component on all pages */}
+        <Navbar />
+        {/* Routes component to define the application's routes */}
+        <Routes>
+            {/* Route for the SearchBooks page (exact path: '/') */}
+            <Route path='/' element={<SearchBooks />} />
+            {/* Route for the SavedBooks page (exact path: '/saved') */}
+            <Route path='/saved' element={<SavedBooks />} />
+            {/* Default route for handling incorrect page URLs */}
+            <Route path='*' element={<h1 className='display-2'>Page Not Found!</h1>} />
+        </Routes>
       </Router>
     </ApolloProvider>
   );
