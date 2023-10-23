1efc01e14ff4aaf92b841150eaa525aa272c33c6
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-10-22 20:31:54 -0400

    update app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
+import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
-// Configure Apollo Client
+// Setup Apollo Client
+const uri = '/graphql';
+const cache = new InMemoryCache();
+
 const client = new ApolloClient({
-  uri: "/graphql", // The URI to the GraphQL server endpoint
-  cache: new InMemoryCache(),
+  uri,
+  cache,
   headers: {
-    authorization: localStorage.getItem("id_token") ? `Bearer ${localStorage.getItem("id_token")}` : "",
+    authorization: `Bearer ${localStorage.getItem('id_token')}` || '',
   },
+  
 });
 
 function App() {
   return (
-    // Wrap the entire application with ApolloProvider to enable Apollo Client functionality for components
     <ApolloProvider client={client}>
-      {/* Use Router component for client-side routing */}
-      <Router>
-        {/* Display the Navbar component on all pages */}
-        <Navbar />
-        {/* Routes component to define the application's routes */}
-        <Routes>
-            {/* Route for the SearchBooks page (exact path: '/') */}
-            <Route path='/' element={<SearchBooks />} />
-            {/* Route for the SavedBooks page (exact path: '/saved') */}
-            <Route path='/saved' element={<SavedBooks />} />
-            {/* Default route for handling incorrect page URLs */}
-            <Route path='*' element={<h1 className='display-2'>Page Not Found!</h1>} />
-        </Routes>
-      </Router>
+      <Navbar />
+      <Outlet />
     </ApolloProvider>
   );
 }
